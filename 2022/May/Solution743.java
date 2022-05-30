4f96fdecb4ceafdf52a19f2e5493a1245041fdeb
hemanth <hemanthjagaari@gmail.com>
2022-05-30 10:11:07 +0530

    m22

--- 2022/May/Solution743.java
+++ 2022/May/Solution743.java
@@ -34,3 +34,42 @@ ui != vi
 0 <= wi <= 100
 All the pairs (ui, vi) are unique. (i.e., no multiple edges.)
 */
+class Solution743 {
+    public int mindist(int[] dist,HashSet<Integer> visit){
+        int index=-1;
+        int min=Integer.MAX_VALUE;
+        for(int i=1;i<dist.length;i++){
+            if(!visit.contains(i) && dist[i]<min){
+                index=i;
+                min=dist[i];
+            }
+        }
+        return index;
+        
+    }
+    public int networkDelayTime(int[][] times, int n, int k) {
+        int[][] adj=new int[n+1][n+1];
+        for(int[] a:adj) Arrays.fill(a,Integer.MAX_VALUE);
+        for(int[] t:times){
+            adj[t[0]][t[1]]=t[2];
+        }
+        int[] dist=new int[n+1];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        HashSet<Integer> visited=new HashSet<>();
+        dist[k]=0;
+        for(int i=1;i<=n;i++){
+            int minindex=mindist(dist,visited);
+            if(minindex==-1) break;
+            visited.add(minindex);
+            for(int j=1;j<=n;j++){
+                int val=adj[minindex][j];
+                if(!visited.contains(j) && adj[minindex][j]!=Integer.MAX_VALUE && (dist[minindex]+val)<dist[j]){
+                    dist[j]=dist[minindex]+val;
+                }
+            }
+        }
+        int max=0;
+        for(int i=1;i<=n;i++) max=Math.max(max,dist[i]);
+        return max==Integer.MAX_VALUE?-1:max;
+    }
+}
